60ed6a17a76481c896e4baa7c31285694dceaa25
Amy Karnaze <akarnaze@gmail.com>
2020-09-17 21:07:06 -0600

    Remove favorite route

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -5,23 +5,19 @@ import { Link } from 'react-router-dom'
 // import PropTypes from 'prop-types'
 
 const PlantCard = (props) => {
-  // console.warn("PPP", props)
   return(
     <section className="plant-card">
       <section className="plants">
       <Link to={{
         pathname:`plants/${props.plant.id}`
       }}>
-      <h1>{props.plant.common_name}</h1>
-      <img className="plant-card-image" alt={props.plant.common_name} src={props.plant.image_url} />
-      <h3>{props.plant.scientific_name}</h3>
+        <h1>{props.plant.common_name}</h1>
+        <img className="plant-card-image" alt={props.plant.common_name} src={props.plant.image_url} />
+        <h3>{props.plant.scientific_name}</h3>
       </Link>
+     <button id={props.plant.id} onClick={props.addFavorite}>Love</button>
       </section>
-     <Link to={{
-       pathname:`favorites/`
-     }}>
-     <button className="love" id={props.plant.id} onClick={props.addFavorite}>Love</button>
-     </Link>
+    
     </section>
   )
 }
