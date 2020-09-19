a496ae2f01590c3704cd05b401293e3edd118e34
Amy Karnaze <akarnaze@gmail.com>
2020-09-19 16:44:39 -0600

    Add on class and conditionally render class

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './PlantCard.css'
 import '../Plants/Plants'
 import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types'
+import fav from '../../assets/fav.png'
+import unfav from '../../assets/unfav.png'
+import PropTypes from 'prop-types'
 
 const PlantCard = (props) => {
   console.log(props)
@@ -16,9 +18,20 @@ const PlantCard = (props) => {
       </Link>
         <h1 className="common-name">{props.plant.common_name}</h1>
         <h3 className="sci-name">{props.plant.scientific_name}</h3>
-     <button id={props.plant.id} onClick={props.handleClick} className="add-button">Love</button>
+        <button id={props.plant.id} onClick={props.handleClick} className={`add-button ${props.isFavorite ? "on" : ""}`}>Love</button>
+        {
+          /*            `banner ${active ? "active" : ""}`
+
+          /*<img id={props.plant.id} onClick={props.handleClick} className='unfav' src={props.isFavorite ? fav : unfav} alt="favorite" />
+           */
+        }
+
+
+
+
     </section>
   )
 }
+
 
 export default PlantCard
