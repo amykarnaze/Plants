cd740eaba1a734f12573864d6d863f3c1eeb611b
Amy Karnaze <akarnaze@gmail.com>
2020-09-18 19:17:33 -0600

    Add fetch call to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,42 +9,69 @@ import Favorites from '../Favorites/Favorites'
 import PlantCard from '../PlantCard/PlantCard'
 import { fetchAllPlants } from '../../API.js'
 
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      // plants: [],
-    }
-    }
-          // <Route path='/favorites/' component={Favorites}></Route>
+      plants: [],
+      favorites: []
+      }
+  }
 
+  componentDidMount = async () => {
+    await fetchAllPlants() 
+    .then(data => this.setState({plants: data.data}))
+    // .then(data => console.log("PLANTS", data.data))
+    .catch(error => alert(error.message))
+  }
+
+  handleClick = async (event) => {
+    const id = Number(event.target.id);
+    console.log("+++", id)
+    const lovedPlant = await this.state.plants.find(plant => plant.id === id)
+    console.log("lovedPlant", lovedPlant)
+    const onList = await this.state.favorites.includes(lovedPlant)
+    // console.log("OnLIST",onList)
+
+    if(!onList) {
+      const newFavorites = [... this.state.favorites, lovedPlant]
+      // console.log("newfav", newFavorites)
+      this.setState({favorites: newFavorites})
+    } else {
+      let removePlant = this.state.favorites.filter(plant => plant !== lovedPlant)
+      this.setState({favorites: removePlant})
+    }
+  }
 
   render () {
-  return (
-    <main>
-      <Header />
-      <Route exact path="/" component={Plants}></Route>
-      <Route path='/plants/:id' 
-        render={(props) =>
-        <PlantInfo 
-        {...props}
-        />}
-      />
-    </main>
-  )
+    return (
+      <section className="App-container">
+        <Header />
+        <section className="actual-plants-container">
+          <h1>PLANTS!</h1>
+          <Route exact path="/">
+          {this.state.plants && <Plants plants={this.state.plants} handleClick={this.handleClick}/>}
+          </Route>
+        </section>
+          <div className="favorites-here-container">
+        <Route path='/favorites/'>
+          <Favorites favorites={this.state.favorites} />
+        </Route>
+          </div>
+        <Route path='/plants/:id' 
+          render={(props) =>
+          <PlantInfo 
+          {...props}
+          />}
+        />
+      </section>
+    )
+  }
 }
-}
-
-/* <Route path='/:plantId' render={({ match }) => {
-      console.log({match: match})
-        const plantClicked = this.state.plants.find((plant) => plant.id == parseInt(match.params.plant.id))
-        return <PlantInfo plant={plantClicked} /> }}
-
-      /> */
-
-// {this.state.plants && <Plants plants={this.state.plants} />}
 
 
 export default App;
+
+ 
