a3112f3b480f6b385f38d63fbaae5f1c2f56d002
Amy Karnaze <akarnaze@gmail.com>
2020-09-19 16:46:04 -0600

    Add psedo for App test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { Component } from 'react'
+import { screen, render, waitFor } from '@testing-library/react';
 import App from './App';
+import { NavLink, Link } from 'react-router-dom'
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { plantPic } from "../../assets/plantPic.png"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App component', () => {
+
+  it.skip('should render Header component when page loads', () => {
+    render(<BrowserRouter><Header /></BrowserRouter>)
+
+  })
+
+  it.skip('should display all plants on page load', () => {
+
+  })
+
+  it.skip('should take the user to the homepage when the home button is clicked', () => {
+    render(<BrowserRouter><Header /></BrowserRouter>)
+
+  })
+
+  it.skip('should take the user to the homepage when the Plant List button is clicked', () => {
+    render(<BrowserRouter><Header /></BrowserRouter>)
+
+  })
+
+
+  
+  
+})
+// <Header /><Route exact path="/" component={Plants}></Route>
+//     <Route path='/plants/:id' 
+//       render={(props) =>
+//       <PlantInfo 
+//       {...props}
+//       />}
+//     />
