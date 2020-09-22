e53e351cf2b8acc2d0d137a44a6d6805216e8980
Amy Karnaze <akarnaze@gmail.com>
2020-09-21 18:13:17 -0600

    Remove isFavorite and lint

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
 import './Plants.css'
-import { fetchAllPlants } from '../../API.js'
 import PlantCard from '../PlantCard/PlantCard'
-import Favorites from '../Favorites/Favorites'
-import { Route, Link, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types'
 
 class Plants extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  isFavorite(plant) {
-    return this.props.favorites ? this.props.favorites.includes(plant.id) : false
-  }
-  isFavorite={this.isFavorite(plant)
 
   displayPlants() {
     return this.props.plants.map(plant => {
       return <PlantCard plant={plant} handleClick={this.props.handleClick} />
     })
   }
-
   render() {
     let plantCards = this.displayPlants()
     return (
