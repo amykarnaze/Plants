13362254eeff8f56cd6f7e8a9c2344c9533ea470
Amy Karnaze <akarnaze@gmail.com>
2020-09-21 18:31:29 -0600

    Lint

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 import './Header.css'
-import { plantPic } from "../../assets/plantPic.png"
 
 const Header = () => {
-    return (
-      <nav className="nav">
-      
+  return (
+    <nav className="nav">
       <ul>
-        <li><NavLink to='/' className='nav-link home-link' aria-label='navigate to home page'>HOME</NavLink></li>
-        <li><NavLink to='/favorites' className='nav-link favorites-link' aria-label='navigate to favorite plants'>YOUR PLANT LIST</NavLink></li>
+        <li><NavLink to='/' className='nav-link link' aria-label='navigate to home page' alt="home">HOME</NavLink></li>
+        <li><NavLink to='/favorites' className='nav-link link' aria-label='navigate to favorite plants' alt="list">YOUR PLANT LIST</NavLink></li>
       </ul>
     </nav>
   )
 }
 
 export default Header
-
-  // <Link className="link-name" to='/'><h1 className="title-name">Plants</h1><img className="img-plant" alt="plant" src={plantPic}/></Link>
-  //       <span className="icon-and-logo">
-  //         <img className='plant-img' alt='plant-pic' src={plantPic} />
-  //         <label className='plant-logo'>Plant Magic</label>
-  //       </span>
