7ae385e6a5e563ea67a8f325864d4c62ec1430a6
Amy Karnaze <akarnaze@gmail.com>
2020-12-22 19:43:23 -0700

    Add image to display if image is not fetched

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom'
 import fav from '../../assets/fav.png'
 import unfav from '../../assets/unfav.png'
 import PropTypes from 'prop-types'
+import salad from '../../assets/salad.png'
 
 const PlantCard = (props) => {
   return(
     <section className="plant-card">
         <img id={props.plant.id} onClick={props.handleClick} className='unfav' src={props.plant.plantLoved ? fav : unfav} alt={props.plant.scientific_name} />
       <Link to={{pathname:`plants/${props.plant.id}` }}>
-        <img className="plant-card-image active" alt={props.plant.common_name} src={props.plant.image_url}/>
+        <img className="plant-card-image active" alt={props.plant.common_name} src={props.plant.image_url ? props.plant.image_url : salad}/>
       </Link>
         <h1 className="common-name">{props.plant.common_name}</h1>
         <h3 className="sci-name">{props.plant.scientific_name}</h3>
