d0ed98c9cbe589bceaea0c839bb2c0d77446e4ca
Amy Karnaze <akarnaze@gmail.com>
2020-12-22 19:57:18 -0700

    Remove unused files

--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { plants } from './setPlants'
-import { combineReducers } from 'redux'
-
-export const rootReducer = combineReducers({plants})
-
--- src/reducers/setPlants.js
+++
@@ -1,8 +0,0 @@
-export const plants = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_PLANTS':
-      return action.plants
-    default:
-      return state
-  }
-}
